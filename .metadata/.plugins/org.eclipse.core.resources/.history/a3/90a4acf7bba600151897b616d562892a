35f0853b0a1d733fcf10379a622f3f9563008730
Tuor <chudi1223@gmail.com>
2015-12-19 20:50:03 -0500

    Blah

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a3/90a4acf7bba600151897b616d562892a
@@ -0,0 +1,5 @@
+package io.github.Tuor;
+
+public class Plugin {
+
+}
--- FirstPlugin/src/io/github/Tuor/Plugin.java
+++ FirstPlugin/src/io/github/Tuor/Plugin.java
@@ -1,5 +1,23 @@
 package io.github.Tuor;
 
-public class Plugin {
+	import org.bukkit.plugin.java.*;
+	import org.bukkit.command.*;
 
-}
+	public class Plugin extends JavaPlugin{
+	 
+	 
+	    @Override
+	    public void onEnable() {
+	     getLogger().info("First Plug In Init");
+	     
+	    }
+	   
+	    @Override
+	    public void onDisable() {
+	    }
+	    
+	    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	  return false;
+	    }
+	}
+
